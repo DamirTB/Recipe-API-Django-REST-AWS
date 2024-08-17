31eb5a77ae3b24efe5eb4540e302968dcca2943c
DamirTB <talipdamir@gmail.com>
2024-08-17 17:42:30 +0500

    Add wait_for_db & tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,17 +1,12 @@
 from django.test import SimpleTestCase
-from rest_framework.test import APIClient
 from app import calc
 
+
 class CaclTests(SimpleTestCase):
-
-    
     def test_add_numbers(self):
-
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
-    
