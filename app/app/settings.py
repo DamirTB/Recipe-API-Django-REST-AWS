81db954b85acff5f478b5b199bb59ac4d72dadce
DamirTB <talipdamir@gmail.com>
2024-08-30 22:20:47 +0500

    add admin for User model, customize admin & make tests for admin actions

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=)zbos-@mod$dsn5w73d*6kim8koo)gr^rd@!^yjz=cfd5+(-u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
