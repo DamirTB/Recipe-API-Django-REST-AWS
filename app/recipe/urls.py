b251b8dc79382d418e0966fe2eeba41aa25a86f9
DamirTB <talipdamir@gmail.com>
2024-10-05 21:38:21 +0500

    new app recipe has been created

---
+++ app/recipe/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from recipe import views
+from rest_framework.routers import DefaultRouter
+
+"""
+URL mapping for the recipe app
+"""
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
